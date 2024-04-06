d59f25bc3518ae1d19afd922b376d2a4731c513f
mylle <myllersilva1310@gmail.com>
2024-04-06 15:51:16 -0300

    create rotes: contact, about

--- webapp/app.py
+++ webapp/app.py
@@ -24,6 +24,15 @@ def plot_matrix(matrix_str):
 def home():
     return render_template('home.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 
 @app.route('/plot_matrix', methods=['GET', 'POST'])
 def plot():
